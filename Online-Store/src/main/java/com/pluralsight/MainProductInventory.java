f3e06dac500d2786f6a3d00316b792f043da0402
MoFunCode <mohamed.sul02@gmail.com>
2025-04-25 12:27:36 -0500

    Add fields, constructor, and accessor methods to Product class

---
+++ Online-Store/src/main/java/com/pluralsight/MainProductInventory.java
@@ -0,0 +1,24 @@
+package com.pluralsight;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+
+public class MainProductInventory {
+
+    public static void main(String[] args) {
+        String filename = "src/main/resources/products.csv";
+
+
+    }
+
+   public void loadProductsFromFile(String filename){
+
+
+
+    }
+
+
+
+
+
+}
---
+++ Online-Store/src/main/java/com/pluralsight/Product.java
@@ -0,0 +1,37 @@
+package com.pluralsight;
+
+public class Product {
+
+    private String sku;
+    private String name;
+    private double price;
+    private String department;
+
+    // Constructor
+    public Product(String sku, String name, double price, String department) {
+        this.sku = sku;
+        this.name = name;
+        this.price = price;
+        this.department = department;
+    }
+
+    // Getters
+    public String getSku() {
+        return sku;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+}
+
+
