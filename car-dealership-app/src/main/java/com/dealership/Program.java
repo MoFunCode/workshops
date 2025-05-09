0a23487edf459e740dfb636cbc0606b4c9cd02df
MoFunCode <mohamed.sul02@gmail.com>
2025-05-09 11:53:19 -0500

    Add Vehicle class with properties and toString override
    Created the Vehicle data container class with:
    - Basic vehicle properties (VIN, year, make, model, type, etc.)
    - Getter/setter methods
    - Custom toString() method for readable output

--- car-dealership-app/src/main/java/com/dealership/Program.java
+++ car-dealership-app/src/main/java/com/dealership/Program.java
@@ -16,4 +16,15 @@ package com.dealership;
 
 public class Program {
 
+    public static void main(String[] args) {
+
+        Vehicle myVehicle = new Vehicle("J123LV21", 2002, "Honda", "Accord",
+                "Car", "Black", 130345,100);
+
+        System.out.println(myVehicle);
+
+
+
+    }
+
 }
